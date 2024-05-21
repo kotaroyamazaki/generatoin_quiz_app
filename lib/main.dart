0b8032fddd3989819ffb8581f240f1a2877dc4a6
KotaroYamazaki <sentoryoku53m@gmail.com>
2024-05-22 01:13:06 +0900

    fidx

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:education_quiz_app/providers/providers.dart';
-import 'package:education_quiz_app/screens/quiz_screen.dart';
+import 'package:education_quiz_app/screens/home_screen.dart';
 import 'package:education_quiz_app/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -17,57 +16,6 @@ class MyApp extends StatelessWidget {
       title: 'あのときなにがあった？クイズ',
       theme: appTheme,
       home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends ConsumerWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final quizService = ref.watch(quizServiceProvider);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('あのときなにがあった？クイズ'),
-        backgroundColor: Colors.indigo,
-      ),
-      body: Container(
-        decoration: backgroundDecoration,
-        child: ListView(
-          padding: const EdgeInsets.all(16.0),
-          children: List.generate(2023 - 2000 + 1, (index) {
-            final year = (2023 - index).toString();
-            return Card(
-              margin: const EdgeInsets.symmetric(vertical: 10.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              elevation: 5.0,
-              child: ListTile(
-                title: Text(
-                  '$year年のクイズ',
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                trailing: const Icon(Icons.arrow_forward),
-                onTap: () async {
-                  final quizzes = await quizService.loadQuizzes(year);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          QuizScreen(year: year, quizzes: quizzes),
-                    ),
-                  );
-                },
-              ),
-            );
-          }),
-        ),
-      ),
     );
   }
 }
