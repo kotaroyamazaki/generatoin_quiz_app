134266df44b99e70b6cdaa857937111333e3aa6e
KotaroYamazaki <sentoryoku53m@gmail.com>
2024-06-11 00:31:46 +0900

    ローディングでぶっこわれるバグを修正

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,6 +18,7 @@ class HomeScreen extends ConsumerStatefulWidget {
 
 class HomeScreenState extends ConsumerState<HomeScreen> {
   late RateMyApp _rateMyApp;
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -47,7 +48,6 @@ class HomeScreenState extends ConsumerState<HomeScreen> {
     final player = AudioPlayer();
     final firestoreService = ref.watch(firestoreServiceProvider);
     final scores = ref.watch(scoreProvider);
-    var isLoading = false;
 
     return Scaffold(
       appBar: AppBar(
@@ -61,80 +61,80 @@ class HomeScreenState extends ConsumerState<HomeScreen> {
         ],
       ),
       body: SafeArea(
-        child: scores.isEmpty || isLoading
-            ? const Center(child: CircularProgressIndicator())
-            : ListView.separated(
-                separatorBuilder: (context, index) => const Divider(
-                  color: grey,
-                  height: 1,
+        child: ListView.separated(
+          separatorBuilder: (context, index) => const Divider(
+            color: grey,
+            height: 1,
+          ),
+          itemCount: maxQuizYear - minQuizYear + 1,
+          itemBuilder: (context, index) {
+            final year = (maxQuizYear - index).toString();
+            final score = scores[year] ?? '--';
+
+            return ListTile(
+              tileColor: Colors.white,
+              leading: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFE8EDF5),
+                  borderRadius: BorderRadius.circular(8.0),
                 ),
-                itemCount: maxQuizYear - minQuizYear + 1,
-                itemBuilder: (context, index) {
-                  final year = (maxQuizYear - index).toString();
-                  final score = scores[year] ?? '--';
-
-                  return ListTile(
-                    tileColor: Colors.white,
-                    leading: Container(
-                      height: 48,
-                      width: 48,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFE8EDF5),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      child: Icon(Icons.emoji_events_outlined,
-                          size: 24,
-                          color: score == maxQuizNum ? Colors.yellow : black),
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 10.0, horizontal: 20.0),
-                    title: Text(
-                      '$year年のクイズ',
-                      style: const TextStyle(
-                        color: black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    subtitle: Text(
-                      '得点: ${score != 0 ? score : "--"}/$maxQuizNum 点',
-                      style: const TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                    trailing: const Icon(Icons.arrow_forward_ios,
-                        color: Colors.black),
-                    onTap: () async {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      final quizzes = await firestoreService.loadQuizzes(year);
-                      if (quizzes.isEmpty) {
-                        if (!context.mounted) return;
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('クイズが見つかりませんでした。'),
-                          ),
-                        );
-                        return;
-                      }
-
-                      setState(() {
-                        isLoading = false;
-                      });
-                      if (!context.mounted) return;
-                      player.play(AssetSource('sounds/select.mp3'));
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              QuizDetailScreen(year: year, quizzes: quizzes),
-                        ),
-                      );
-                    },
-                  );
-                },
+                child: Icon(Icons.emoji_events_outlined,
+                    size: 24,
+                    color: score == maxQuizNum ? Colors.yellow : black),
               ),
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              title: Text(
+                '$year年のクイズ',
+                style: const TextStyle(
+                  color: black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                '得点: ${score != 0 ? score : "--"}/$maxQuizNum 点',
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              trailing:
+                  const Icon(Icons.arrow_forward_ios, color: Colors.black),
+              onTap: () async {
+                if (isLoading) return;
+                setState(() {
+                  isLoading = true;
+                });
+
+                final quizzes = await firestoreService.loadQuizzes(year);
+                if (quizzes.isEmpty) {
+                  if (!context.mounted) return;
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('クイズが見つかりませんでした。'),
+                    ),
+                  );
+                  return;
+                }
+
+                setState(() {
+                  isLoading = false;
+                });
+                if (!context.mounted) return;
+                player.play(AssetSource('sounds/select.mp3'));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        QuizDetailScreen(year: year, quizzes: quizzes),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
